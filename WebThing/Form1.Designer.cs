ec8ed0d491bba001f37bc5df13edeba554c8e466
nscho <nscho@MSI>
2022-09-08 14:48:22 -0400

    Added Stuff

--- WebThing/Form1.Designer.cs
+++ WebThing/Form1.Designer.cs
@@ -30,18 +30,21 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 55);
+            this.button1.Location = new System.Drawing.Point(134, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -53,72 +56,105 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(776, 23);
+            this.textBox1.Size = new System.Drawing.Size(368, 23);
             this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(646, 360);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(142, 69);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(96, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.Size = new System.Drawing.Size(0, 36);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Title";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(96, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.Size = new System.Drawing.Size(0, 36);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Author";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(96, 134);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.Size = new System.Drawing.Size(0, 36);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Duration";
             // 
-            // checkBox1
+            // label4
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(713, 84);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(58, 19);
-            this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "Audio";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(30, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Title:";
             // 
-            // checkBox2
+            // label5
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(713, 109);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(56, 19);
-            this.checkBox2.TabIndex = 7;
-            this.checkBox2.Text = "Video";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Creator:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Time:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(386, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "✓";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(386, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(41, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Help";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mp4",
+            "mp3",
+            "wav"});
+            this.comboBox1.Location = new System.Drawing.Point(295, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(85, 23);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(713, 134);
+            this.button2.Location = new System.Drawing.Point(214, 42);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Select Locationm";
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Audio Only";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -126,18 +162,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(442, 201);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Youtube Converter";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,12 +187,15 @@
 
         private Button button1;
         private TextBox textBox1;
-        private TextBox textBox2;
         private Label label1;
         private Label label2;
         private Label label3;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button3;
+        private Button button4;
+        private ComboBox comboBox1;
         private Button button2;
     }
 }
--- WebThing/Form1.cs
+++ WebThing/Form1.cs
@@ -1,8 +1,9 @@
-using HtmlAgilityPack;
 using System.Linq.Expressions;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 using YoutubeExplode.Converter;
+using YoutubeExplode.Common;
+using System.Windows.Forms;
 
 namespace WebThing
 {
@@ -19,40 +20,26 @@ namespace WebThing
             {
                 var youtube = new YoutubeClient();
 
-                // You can specify both video ID or URL
+                // You can specify both video ID or URL "https://www.youtube.com/watch?v=UmioPP3WD0w"
                 var video = await youtube.Videos.GetAsync(textBox1.Text);
 
                 var title = video.Title;
                 var author = video.Author.ChannelTitle;
                 var duration = video.Duration;
-                label1.Text = (title).ToString();
-                label2.Text = (author).ToString();
-                label3.Text = (duration).ToString();
+                label4.Text = (title).ToString();
+                label5.Text = (author).ToString();
+                label6.Text = (duration).ToString();
                 var streamManifest = await youtube.Videos.Streams.GetManifestAsync(textBox1.Text);
-                if (checkBox1.Checked) ;
-                {
-                    var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-                    var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-                    await youtube.Videos.DownloadAsync(textBox1.Text, title + ".mp3", o => o
-                    .SetContainer("mp3") // override format
-                );
-                }
-                if (checkBox2.Checked) ;
-                {
-                    var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
-                    var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-                    await youtube.Videos.DownloadAsync(textBox1.Text, title + ".mp4", o => o
-                    .SetContainer("mp4") // override format
-                );
-                }
-                if (checkBox1.Checked & !checkBox2.Checked) ;
-                {
-                    textBox2.Text = "ONLY SELECT ONE BOX";
-                }
+                var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                await youtube.Videos.DownloadAsync(textBox1.Text, "video.mp4", o => o
+                .SetContainer("mp4") // override format
+                .SetPreset(ConversionPreset.UltraFast) // change preset
+                .SetFFmpegPath(@"C:\ffmpeg\bin\ffmpeg.exe")); // custom FFmpeg location
             }
             catch
             {
-                textBox2.Text = "Invalid URL";
+                System.Windows.Forms.MessageBox.Show("Invalid Link/ID. Click Help for more info.");
             }
         }
 
@@ -61,9 +48,55 @@ namespace WebThing
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button3_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            try
+            {
+                var youtube = new YoutubeClient();
+                var video = await youtube.Videos.GetAsync(textBox1.Text);
+                var title = video.Title;
+                var author = video.Author.ChannelTitle;
+                var duration = video.Duration;
+                label4.Text = (title).ToString();
+                label5.Text = (author).ToString();
+                label6.Text = (duration).ToString();
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Invalid Link/ID. Click Help for more info.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Download - Downloads the video + Sounds\n\n Download Audio Only - Should be self explanatory\n\n The Check mark grabs the video Metadata and displays it for you\n\nALL VIDEOS ARE SAVED TO THE DIRECTORY THAT THIS APP IS IN");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var youtube = new YoutubeClient();
+
+            // You can specify both video ID or URL "https://www.youtube.com/watch?v=UmioPP3WD0w"
+            var video = await youtube.Videos.GetAsync(textBox1.Text);
+
+            var title = video.Title;
+            var author = video.Author.ChannelTitle;
+            var duration = video.Duration;
+            label4.Text = (title).ToString();
+            label5.Text = (author).ToString();
+            label6.Text = (duration).ToString();
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(textBox1.Text);
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+            await youtube.Videos.DownloadAsync(textBox1.Text, "video." + comboBox1.Text, o => o
+            .SetContainer(comboBox1.Text) // override format
+            .SetPreset(ConversionPreset.UltraFast) // change preset
+            .SetFFmpegPath(@"C:\ffmpeg\bin\ffmpeg.exe")); // custom FFmpeg location
         }
     }
 }
